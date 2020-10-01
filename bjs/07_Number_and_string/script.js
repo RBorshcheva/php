83c0b8347a7608de8cde0f9958ec19b28dba5766
RBorshcheva <raisaborshcheva@mail.ru>
2020-10-01 08:46:11 +0300

    zero on main table

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -5,9 +5,10 @@ const inputWindow = document.getElementById('inputWindow');
 
 
 document.getElementById('btn_clr').addEventListener('click', function () {
+    inputWindow.value = '';
     lastOperand = 0;
     operation = null;
-    inputWindow.value = '';
+    inputWindow.value = '0';
 })
 // all buttons of calc
 document.getElementById('btn_0').addEventListener('click', function () {
@@ -15,38 +16,65 @@ document.getElementById('btn_0').addEventListener('click', function () {
 })
 
 document.getElementById('btn_1').addEventListener('click', function () {
+    if(inputWindow.value == 0){
+        inputWindow.value ='';
+    } ;
     inputWindow.value += '1';
 })
 
 document.getElementById('btn_2').addEventListener('click', function () {
+    if(inputWindow.value == 0){
+        inputWindow.value ='';
+    } ;
     inputWindow.value += '2';
 })
 
 document.getElementById('btn_3').addEventListener('click', function () {
+    if(inputWindow.value == 0){
+        inputWindow.value ='';
+    } ;
     inputWindow.value += '3';
 })
 
 document.getElementById('btn_4').addEventListener('click', function () {
+    if(inputWindow.value == 0){
+        inputWindow.value ='';
+    } ;
     inputWindow.value += '4';
 })
 
 document.getElementById('btn_5').addEventListener('click', function () {
+    if(inputWindow.value == 0){
+        inputWindow.value ='';
+    } ;
     inputWindow.value += '5';
 })
 
 document.getElementById('btn_6').addEventListener('click', function () {
+    if(inputWindow.value == 0){
+        inputWindow.value ='';
+    } ;
     inputWindow.value += '6';
 })
 
 document.getElementById('btn_7').addEventListener('click', function () {
+    if(inputWindow.value == 0){
+        inputWindow.value ='';
+    } ;
     inputWindow.value += '7';
 })
 
 document.getElementById('btn_8').addEventListener('click', function () {
+    if(inputWindow.value == 0){
+        inputWindow.value ='';
+    } ;
     inputWindow.value += '8';
 })
 
 document.getElementById('btn_9').addEventListener('click', function () {
+    if(inputWindow.value == 0){
+        inputWindow.value ='';
+    } ;
     inputWindow.value += '9';
 })
 
